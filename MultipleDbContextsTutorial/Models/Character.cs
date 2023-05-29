f4be7bff9f41f0ce2ac4f0bf55935983c29ea182
Patrick <patrick@godknows.de>
2023-05-29 16:12:15 +0200

    Add project files.

---
+++ MultipleDbContextsTutorial/Models/Character.cs
@@ -0,0 +1,10 @@
+﻿namespace MultipleDbContextsTutorial.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int Hitpoints { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ MultipleDbContextsTutorial/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MultipleDbContextsTutorial.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public List<Character> Characters { get; set; } = new List<Character>();
+    }
+}
